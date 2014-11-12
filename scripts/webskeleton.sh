7b6c0f6386bceb0ad64690bffb2754e22c0a03ff
Sebastian Veggiani <sveggiani@gmail.com>
2014-11-12 00:13:31 -0300

    My first dotfiles commit ^^

---
+++ scripts/webskeleton.sh
@@ -0,0 +1,39 @@
+echo "/* Creating project web skeleton. v5.0"
+echo "------------------------------------------------------ */"
+
+mkdir material
+echo "|_ material"
+
+mkdir material/definition
+echo "  |_ definition"
+
+mkdir material/design
+echo "  |_ design"
+mkdir material/design/designs
+echo "    |_ designs"
+mkdir material/design/pictures
+echo "    |_ pictures"
+mkdir material/design/fonts
+echo "    |_ fonts"
+
+mkdir material/media
+echo "  |_ media"
+
+mkdir material/client
+echo "  |_ client"
+mkdir material/client/estimates
+echo "    |_ estimates"
+
+mkdir backups
+echo "|_ backups"
+mkdir backups/webroot
+echo "  |_ webroot"
+mkdir backups/databases
+echo "  |_ databases"
+
+mkdir webroot
+echo "|_ webroot"
+
+echo
+echo 'done....'
+
