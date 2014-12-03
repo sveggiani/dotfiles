3a4aece01dbc7116ebb9af2dedb97b9e219d9954
Sebastian Veggiani <sveggiani@gmail.com>
2014-12-03 20:14:53 -0300

    Added Windows webskeleton create script

---
+++ scripts/webskeleton_windows.bat
@@ -0,0 +1,23 @@
+md material
+cd material
+	md definition
+	md design
+		cd designs
+		md pictures
+		md typography
+    md media
+		cd..
+
+	md client
+		cd client
+		md estimates
+		cd..
+	cd..
+
+md backup
+	cd backup
+	md webroot
+	md ddbb
+	cd..
+
+md webroot
