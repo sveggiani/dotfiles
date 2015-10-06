62fb17eef212fed573dfbb401627ba4ea4cd6aae
Sebastian Veggiani <sveggiani@gmail.com>
2015-10-06 13:07:41 -0300

    Added script to update npm and homebrew packages list

---
+++ scripts/update_installed_packages_list.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update node (npm) packages list
+/usr/local/bin/npm list -g --depth=0 > /Users/sebastian/.npm_installed_packages
+
+# Update homebrew packages list
+/usr/local/bin/brew list > /Users/sebastian/.homebrew_installed_packages
+
+# Notify execution
+osascript -e 'display notification "message" with title "Packages list updated" subtitle "npm and homebrew packages lists have been updated"'
