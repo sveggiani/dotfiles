e57ce1c464e94aa7307c27bce3cb318d56f8efb5
Sebastian Veggiani <sveggiani@gmail.com>
2017-12-14 21:55:04 -0300

    Update script to export list of packages

--- scripts/update_installed_packages_list.sh
+++ scripts/update_installed_packages_list.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Add node to path
+export PATH=/usr/local/bin/node:$PATH
+
 # Update node (npm) packages list
-/usr/local/bin/npm list -g --depth=0 > /Users/sebastian/.npm_installed_packages
+/usr/local/bin/npm list -g --depth=0 >/Users/sebastian/.npm_installed_packages 2>/Users/sebastian/.npm_installed_packages.log
 
 # Update homebrew packages list
-/usr/local/bin/brew list > /Users/sebastian/.homebrew_installed_packages
+/usr/local/bin/brew list >/Users/sebastian/.homebrew_installed_packages 2>/Users/sebastian/.homebrew_installed_packages.log
 
 # Notify execution
 osascript -e 'display notification "message" with title "Packages list updated" subtitle "npm and homebrew packages lists have been updated"'
